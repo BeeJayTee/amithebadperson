e3aea5f12c59d1a3cb94b407d76a60aa3fe17ccc
BeeJayTee <91142693+BeeJayTee@users.noreply.github.com>
2023-02-04 11:24:15 -0500

    initialized navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Content from "./components/home/Content";
+import Navbar from "./components/home/NavBar";
 
 function App() {
   return (
     <div className="w-screen h-screen bg-[#f8f9fa]">
+      <Navbar />
       <Content />
     </div>
   );
---
+++ src/components/home/NavBar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <p>poop</p>
+    </div>
+  );
+};
+
+export default Navbar;
