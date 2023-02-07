83e5c6ae8db96cac580a5e385322db44beb9e247
BeeJayTee <91142693+BeeJayTee@users.noreply.github.com>
2023-02-07 10:12:12 -0500

    updated tailwind config

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -14,23 +14,47 @@ const Post = () => {
         setTitle(json.title);
         setContent(json.content);
       }
-      console.log(json);
     };
     fetchPost();
   }, []);
 
+  const handleClick = (e) => {
+    const str = e.target.innerHTML;
+    if (str.startsWith("not")) {
+      console.log("not the bad person");
+    } else if (str.startsWith("is")) {
+      console.log("is the bad person");
+    }
+  };
+
   return (
     <div className="Post">
-      <div className="container m-auto mt-16 max-w-5xl bg-slate-200 px-32 py-4">
-        <div className="rating">
-          <p>NTBP</p>
-          <p>YTBP</p>
-        </div>
-        <div className="title text-center text-2xl mb-8">
-          <h1>{title}</h1>
-        </div>
-        <div className="content">
-          <p>{content}</p>
+      <div className="container m-auto mt-16 max-w-5xl">
+        <div className="content bg-slate-100 px-8 md:px-8 py-4 flex justify-between">
+          <div className="title-content">
+            <h1 className="text-3xl mb-4">{title}</h1>
+            <div className="rating flex gap-4 text-center">
+              <p
+                className="text-xs border border-slate-400 hover:border-[#2b2d42] hover:cursor-pointer p-2"
+                onClick={(e) => handleClick(e)}
+              >
+                not the
+                <br />
+                bad person
+              </p>
+              <p
+                className="text-xs border border-slate-400 hover:border-[#d90429] hover:cursor-pointer p-2"
+                onClick={(e) => handleClick(e)}
+              >
+                is the
+                <br />
+                bad person
+              </p>
+            </div>
+          </div>
+          <div className="body w-1/2">
+            <p>{content}</p>
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+const fontFamily = defaultTheme.fontFamily;
+fontFamily["sans"] = [
+  "Open Sans", // <-- Roboto is a default sans font now
+  "system-ui",
+  // <-- you may provide more font fallbacks here
+];
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: fontFamily, // <-- this is where the override is happening
     extend: {},
   },
   plugins: [require("daisyui")],
